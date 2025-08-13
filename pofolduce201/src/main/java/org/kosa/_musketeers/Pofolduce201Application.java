b75e8ef008ad0f1f8d309aa9f4c203b54fd7042e
sangwook1199 <sangwook1199@gmail.com>
2025-08-13 12:07:00 +0900

    chore: 프로젝트 생성

---
+++ pofolduce201/src/main/java/org/kosa/_musketeers/Pofolduce201Application.java
@@ -0,0 +1,13 @@
+package org.kosa._musketeers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pofolduce201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pofolduce201Application.class, args);
+	}
+
+}
---
+++ pofolduce201/src/test/java/org/kosa/_musketeers/Pofolduce201ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kosa._musketeers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pofolduce201ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
