9d42b7513d57bc87b0e1f6037b28f68ba6e47f62
khgeung <khgeung5357@gmail.com>
2025-08-22 17:44:51 +0900

    fix : mapper 어노테이션 추가

--- pofolduce201/src/main/java/org/kosa/_musketeers/mapper/RecruitMapper.java
+++ pofolduce201/src/main/java/org/kosa/_musketeers/mapper/RecruitMapper.java
@@ -3,6 +3,9 @@ package org.kosa._musketeers.mapper;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface RecruitMapper {
 
 	void saveRecruit(String title, String company, String content, String link, String imgLink);
